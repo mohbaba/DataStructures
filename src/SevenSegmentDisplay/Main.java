439bdc73c0f91f3883b2fa1aa7910a1c9c364c7b
mohbaba <babamu09@gmail.com>
2024-03-13 09:15:28 +0100

    Seven Segment

--- src/SevenSegmentDisplay/Main.java
+++ src/SevenSegmentDisplay/Main.java
@@ -4,12 +4,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        SevenSegmentDisplay display = new SevenSegmentDisplay();
         Scanner scanner = new Scanner(System.in);
         try {
             System.out.println("Enter combination you'd like to display: ");
             String input = scanner.next();
-            System.out.println(display.display(input));
+            System.out.println(SevenSegmentDisplay.display(input));
         }catch(Exception e){
             System.out.println(e.getMessage());
         }
